3acb50ebd2233dfc150093254fc368eedb0e8e46
etileyli <etileyli@hotmail.com>
2021-10-23 20:52:23 +0300

    New file

--- HelloWorld/Account.java
+++ HelloWorld/Account.java
@@ -1,5 +1,19 @@
 public class Account{
   private String name;
+  private double balance;
+
+  public Account(){
+    name = "";
+    balance = 0.0;
+  }
+
+  public Account(String name, double balance){
+    this.name = name;
+
+    if (balance > 0){
+      this.balance = balance;
+    }
+  }
 
   public void setName(String name){
     this.name = name;
@@ -7,5 +21,14 @@ public class Account{
 
   public String getName(){
     return name;
+  }
+
+  public void deposit(double depositAmount){
+    if (depositAmount > 0.0)
+      balance += depositAmount;
+  }
+
+  public double getBalance(){
+    return balance;
   }
 }
---
+++ HelloWorld/AccountTest2.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class AccountTest2{
+  public static void main(String[] args) {
+    Account account1 = new Account("Emre", -7.53);
+    Account account2 = new Account("Ebru", 50);
+
+    // display initial balance of each object
+    System.out.printf("%s balance: $%.2f%n",
+    account1.getName(), account1.getBalance());
+    System.out.printf("%s balance: $%.2f%n%n",
+    account2.getName(), account2.getBalance());
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter deposit amount for account1: "); // prompt
+    double depositAmount = input.nextDouble(); // obtain user input
+    System.out.printf("%nadding %.2f to account1 balance%n%n",
+       depositAmount);
+    account1.deposit(depositAmount); // add to account1's balance
+
+    // display balances
+    System.out.printf("%s balance: $%.2f%n",
+       account1.getName(), account1.getBalance());
+    System.out.printf("%s balance: $%.2f%n%n",
+       account2.getName(), account2.getBalance());
+
+    System.out.print("Enter deposit amount for account2: "); // prompt
+    depositAmount = input.nextDouble(); // obtain user input
+    System.out.printf("%nadding %.2f to account2 balance%n%n",
+       depositAmount);
+    account2.deposit(depositAmount); // add to account2 balance
+
+    // display balances
+    System.out.printf("%s balance: $%.2f%n",
+       account1.getName(), account1.getBalance());
+    System.out.printf("%s balance: $%.2f%n%n",
+       account2.getName(), account2.getBalance());
+  }
+}
