b49ee869116d477619746fe02134a500f26dd3a6
etileyli <etileyli@hotmail.com>
2022-07-30 12:54:51 +0300

    test commit

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,5 +1,24 @@
 public class HelloWorld{
   public static void main(String[] args){
-    System.out.println("Hello World!");
+
+    double cash  = 10000;
+    double busd = 14.07;
+
+    double cashUSD = cash / busd;
+
+    double coinUnitPriceInBinance = 1;   // In usdt
+    double coinUnitPriceInParibu = 13.88;   // In try
+
+
+    double coinAmount = cashUSD / coinUnitPriceInBinance;
+
+    double coinTotalPriceInParibu = coinAmount * coinUnitPriceInParibu;
+
+    double balanceInPercent = (coinTotalPriceInParibu / cash - 1) * 100 ;
+
+    System.out.println("");
+    System.out.println("Total Final Amount\t" + coinTotalPriceInParibu);
+    System.out.println("Balance %" +  balanceInPercent);
+    System.out.println("");
   }
 }
